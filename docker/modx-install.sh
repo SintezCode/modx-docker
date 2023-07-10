1cc78b88c0822e7a04c574e9802950c75a315f02
SintezCode <unarmedf@gmail.com>
2023-07-10 16:03:12 +0300

    change default language to ru

--- docker/modx-install.sh
+++ docker/modx-install.sh
@@ -7,7 +7,7 @@ docker exec -t $(docker ps --filter name=$NAME -q) bash -c "gitify modx:download
 
 docker exec -t $(docker ps --filter name=$NAME -q) bash -c "php setup/cli-install.php --database_server=mariadb \
   --database=$MARIADB_DATABASE --database_user=$MARIADB_USERNAME --database_password=$MARIADB_PASSWORD \
-  --table_prefix=modx_ --language=en --cmsadmin=admin --cmspassword=adminadmin --cmsadminemail=admin@localhost \
+  --table_prefix=modx_ --language=ru --cmsadmin=admin --cmspassword=adminadmin --cmsadminemail=admin@localhost \
   --context_mgr_path=/modx/manager/ --context_mgr_url=/manager/ \
   --context_connectors_path=/modx/connectors/ --context_connectors_url=/connectors/ \
   --context_web_path=/modx/"
