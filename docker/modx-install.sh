adfe40d971315af32b87c78f03f4308d15d978b3
SintezCode <unarmedf@gmail.com>
2023-07-11 16:54:25 +0300

    Change modx version to 3.0.3-pl Add variable to env with modx version

--- docker/modx-install.sh
+++ docker/modx-install.sh
@@ -3,7 +3,7 @@
 source "./.env"
 NAME=${COMPOSE_PROJECT_NAME}-php-fpm
 
-docker exec -t $(docker ps --filter name=$NAME -q) bash -c "gitify modx:download 2.8.4-pl"
+docker exec -t $(docker ps --filter name=$NAME -q) bash -c "gitify modx:download $MODX_VERSION"
 
 docker exec -t $(docker ps --filter name=$NAME -q) bash -c "php setup/cli-install.php --database_server=mariadb \
   --database=$MARIADB_DATABASE --database_user=$MARIADB_USERNAME --database_password=$MARIADB_PASSWORD \
