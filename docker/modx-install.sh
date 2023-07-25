40f84a8d58f1a1697790892fc3186e1bdbbcc190
SintezCode <unarmedf@gmail.com>
2023-07-25 21:42:06 +0300

    Add http_host as DOMAIN to install options Remove setting YANDEX_MAP_API_KEY to _data

--- docker/modx-install.sh
+++ docker/modx-install.sh
@@ -16,6 +16,7 @@ if [ $MODSTORE_API_KEY ]; then
     printf $MODSTORE_API_KEY > ../modx/.modstore.pro.key
 fi
 
+: '
 if [ $YANDEX_MAP_API_KEY ]; then
     mkdir -p "../modx/_data/system_settings/"
     printf "key: yandex_coords_tv_api_key
@@ -23,15 +24,14 @@ value: $YANDEX_MAP_API_KEY
 namespace: yandexcoordstv
 area: api" > ../modx/_data/system_settings/yandex-coords-tv-api-key.yaml
 fi
-
-
+'
 
 docker exec -t $(docker ps --filter name=$NAME -q) bash -c "php setup/cli-install.php --database_server=mariadb \
   --database=$MARIADB_DATABASE --database_user=$MARIADB_USERNAME --database_password=$MARIADB_PASSWORD \
   --table_prefix=$MODX_TABLE_PREFIX --language=$MODX_LANGUAGE --cmsadmin=$MODX_ADMIN_LOGIN --cmspassword=$MODX_ADMIN_PWD --cmsadminemail=$MODX_ADMIN_EMAIL \
   --context_mgr_path=/modx/${MODX_PUBLIC_DIR}${MODX_MGR_DIR} --context_mgr_url=/$MODX_MGR_DIR \
   --context_connectors_path=/modx/${MODX_PUBLIC_DIR}${MODX_CONNECTORS_DIR} --context_connectors_url=/$MODX_CONNECTORS_DIR \
-  --context_web_path=/modx/$MODX_PUBLIC_DIR"
+  --context_web_path=/modx/$MODX_PUBLIC_DIR --http_host=$DOMAIN"
 
 docker exec -t $(docker ps --filter name=$NAME -q) bash -c "cp /modx/${MODX_PUBLIC_DIR}config.core.php /modx/"
 
